e04af586554374b7bc3f1459d4e025ae37ded95a
DanPlayz0 <damuffin36@gmail.com>
2022-11-18 03:13:47 +0000

    More stuff

--- config-private.example.js
+++ config-private.example.js
@@ -1,7 +1,10 @@
 module.exports = {
   token: "DISCORD_BOT_TOKEN",
 
+  // Set 'mongo_uri' to null to disable the MongoDB connection.
   mongo_uri: "mongodb://username:password@localhost:27017/discordbot?authSource=admin&retryWrites=true&ssl=false",
+
+  // Set 'redis.host' to null to disable the Redis connection.
   redis: {
     host: "localhost",
     port: 6379,
