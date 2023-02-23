376b9b4032a12a212d7332ec4f041a7d99531638
DanPlayz0 <damuffin36@gmail.com>
2023-02-23 11:54:42 -0800

    Auto Detect Command Changes and Publish

--- events/ready.js
+++ events/ready.js
@@ -10,20 +10,40 @@ module.exports = class extends Event {
     console.log(`Logged in as ${client.user.tag}`);
 
     async function setupInit() {
-      // Set the game as the "Watching for tags"
-      client.user.setActivity(`servers • /help`, { type: "WATCHING" });
+      // Set the game as the "Watching github • /help"
+      client.user.setActivity(`github • /help`, { type: 4 });
     }
 
     setupInit();
     this.activityInterval = setInterval(setupInit, 90000);
 
-    // Setup the website.
+    // Setup the REST api.
     if((!client.shard || !client.shardId) && client.config.restapi.port != null) {
       client.site = new (require("@structures/restapi/index.js"))(client);
       client.site.listen(client.config.restapi.port);
     }
     
-    // if(client.guilds.cache.has('783178035322159156')) client.guilds.cache.get('783178035322159156').commands.set(client.commands.map(m=>m.commandData))
-    // client.application.commands.set(client.commands.map(m=>m.commandData));
+    // Setup slash commands and update if change detected.
+    const checkApplicationCommands = async (client) => {
+      const clientCommands = await client.application.commands.fetch();
+      client.savedCommands = clientCommands;
+      const commandsDiff = [...Object.values(clientCommands.toJSON()).filter((command) => {
+          const cmd = client.commands.get(command.name)?.commandData;
+          if(!cmd) return true;
+          if(command.description !== cmd.description) return true;
+          if(command.options.length !== cmd.options.length) return true;
+          const discordOptions = command.options.map(x=>`${x.type}|${x.name}|${x.description}|${x.required}|${x.choices?.length}|${x.options?.length}`);
+          const localOptions = cmd.options.map(x=>`${x.type}|${x.name}|${x.description}|${x.required??false}|${x.choices?.length}|${x.options?.length}`);
+          if(discordOptions.filter((x,i) => localOptions[i] !== x).length) return true;
+          return false;
+        }),
+        ...[...client.commands.keys()].filter(x=> !Object.values(clientCommands.toJSON()).some(a => a.name === x))
+      ]
+      if(commandsDiff.length) {
+        client.savedCommands = await client.application.commands.set(client.commands.map(x=>x.commandData));
+        console.log('Detected a change, posting slash commands.');
+      }
+    };
+    checkApplicationCommands(client);
   }
 }
