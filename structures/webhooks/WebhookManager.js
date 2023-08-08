8f9ea3ed2727f7ba405e0d3701c82fc6252a60eb
DanPlayz0 <damuffin36@gmail.com>
2023-08-07 22:02:12 -0700

    config/webhook updates

--- structures/webhooks/WebhookManager.js
+++ structures/webhooks/WebhookManager.js
@@ -1,12 +1,54 @@
-const WebhookClient = require('./WebhookClient');
+const { EmbedBuilder, WebhookClient: DiscordWebhookClient } = require('discord.js');
+
 module.exports = class WebhookManager {
   constructor(client) {
     this.client = client;
-    
 
-    for(let webhook of client.config.webhooks) {
-      if(webhook.id && webhook.token || webhook.url) this[webhook.name] = new WebhookClient(this.client, webhook);
-      else this[webhook.name] = { send: (content) => console.log(`[WEBHOOK - ${webhook.name}]`, content?.content ?? content) };
+    if(!client.config.webhooks) throw TypeError("Config is missing 'webhooks' property");
+    for(const webhookName of ["shard", "error", "guilds", "command"]) {
+      const webhookConfig = client.config?.webhooks?.[webhookName];
+      if (!webhookConfig) continue;
+      if (!this.checkValid(webhookConfig.url)) webhookConfig.url = undefined;
+      if (!this.checkValid(webhookConfig.id)) webhookConfig.id = undefined;
+      if (!this.checkValid(webhookConfig.token)) webhookConfig.token = undefined;
+
+      if (webhookConfig.id && webhookConfig.token || webhookConfig.url) this[webhookName] = new WebhookClient(this.client, webhookName, webhookConfig);
+      else this[webhookName] = { send: (content) => console.log(`[WEBHOOK - ${webhookName}]`, content?.content ?? content) };
     }
+  }
+
+  checkValid(config) {
+    if(config == null) return false;
+    if(config == "null") return false;
+    if(config == undefined) return false;
+    if(config == "undefined") return false;
+    if(config == "") return false;
+    if(config == " ") return false;
+    if(config == "WEBHOOK_URL") return false;
+    if(config == "WEBHOOK_ID") return false;
+    if(config == "WEBHOOK_TOKEN") return false;
+    return true;
+  }
+}
+
+class WebhookClient {
+  constructor(client, name, webhookConfig) {
+    this.client = client;
+    if(!webhookConfig) return {send: ()=>{ console.error(`Config is missing property 'webhooks.${name}'`) }};
+    this.webhook = new DiscordWebhookClient(webhookConfig);
+  }
+
+  async send(content, options) {
+    if (!content) throw ReferenceError('content is not defined');
+    if (typeof content == 'object' && !(content instanceof EmbedBuilder)) { options = content; content = undefined; }
+    if (options instanceof EmbedBuilder) options = { embeds: [options] };
+
+    const newOptions = Object.assign({
+      content: typeof content === 'string' ? content : undefined,
+      embeds: content instanceof EmbedBuilder ? [content] : [],
+      allowedMentions: { repliedUser: false, parse: [] },
+    }, options);
+
+    return this.webhook.send(newOptions);
   }
 }
